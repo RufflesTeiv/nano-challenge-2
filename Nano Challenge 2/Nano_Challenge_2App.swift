61034ad926ad9eea9ebd5539323650a5ad320120
Rafael Teivfik MacOS <rafael.teivfik2@gmail.com>
2023-08-02 15:28:48 -0300

    First commit

---
+++ Nano Challenge 2/Nano_Challenge_2App.swift
@@ -0,0 +1,17 @@
+//
+//  Nano_Challenge_2App.swift
+//  Nano Challenge 2
+//
+//  Created by Rafa (Ruffles) on 02/08/23.
+//
+
+import SwiftUI
+
+@main
+struct Nano_Challenge_2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
